c2cb013d0f684338c9dd9eb1786666424514c750
gowther <heartgowther@gmail.com>
2025-08-12 15:26:43 +0300

    [nvim] remove: session autocmd

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -33,34 +33,3 @@ vim.api.nvim_create_autocmd("BufReadPre", {
     end
   end,
 })
-
--- Project-based session management
-local function get_session_file()
-  local cwd = vim.fn.getcwd()
-  local session_dir = vim.fn.stdpath("data") .. "/sessions"
-  vim.fn.mkdir(session_dir, "p")
-  return session_dir .. "/" .. vim.fn.substitute(cwd, "/", "%%", "g") .. ".vim"
-end
-
--- Auto-save session on exit for projects with git
-vim.api.nvim_create_autocmd("VimLeavePre", {
-  callback = function()
-    if vim.fn.isdirectory(".git") == 1 then
-      local session_file = get_session_file()
-      vim.cmd("mksession! " .. session_file)
-    end
-  end,
-})
-
--- Auto-load session for git projects
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    -- Only auto-load if no arguments passed to nvim
-    if vim.fn.argc() == 0 and vim.fn.isdirectory(".git") == 1 then
-      local session_file = get_session_file()
-      if vim.fn.filereadable(session_file) == 1 then
-        vim.cmd("source " .. session_file)
-      end
-    end
-  end,
-})
