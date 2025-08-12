00863228dc52c8e4b756606f620c04444a2927d3
gowther <heartgowther@gmail.com>
2025-08-12 15:26:18 +0300

    [nvim] upd: set v0.11 lsp format

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -25,6 +25,7 @@ return {
         vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
         vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+
         vim.keymap.set("n", "<leader>wl", function()
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
         end, opts)
@@ -35,74 +36,5 @@ return {
       end,
     })
 
-    local border = {
-      { "┌", "FloatBorder" },
-      { "─", "FloatBorder" },
-      { "┐", "FloatBorder" },
-      { "│", "FloatBorder" },
-      { "┘", "FloatBorder" },
-      { "─", "FloatBorder" },
-      { "└", "FloatBorder" },
-      { "│", "FloatBorder" },
-    }
-    -- LSP settings (for overriding per client)
-    local handlers = {
-      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-      ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
-    }
-
-    local lsp_capabilities = require("blink.cmp").get_lsp_capabilities()
-
-    -- Enhanced LSP caching
-    local default_config = {
-      capabilities = lsp_capabilities,
-      handlers = handlers,
-      flags = {
-        debounce_text_changes = 150,
-        allow_incremental_sync = true,
-      },
-    }
-
-    nvim_lsp.pyright.setup(vim.tbl_deep_extend("force", default_config, {
-      settings = {
-        pyright = { disableOrganizeImports = true },
-        python = {
-          analysis = {
-            diagnosticMode = "openFilesOnly",
-            ignore = { "*" },
-            useLibraryCodeForTypes = false, -- Better performance
-            autoImportCompletions = true,
-          },
-        },
-      },
-    }))
-
-    -- Language servers with caching
-    nvim_lsp.gopls.setup(vim.tbl_deep_extend("force", default_config, {
-      settings = {
-        gopls = {
-          experimentalPostfixCompletions = true,
-          analyses = { unusedparams = true },
-          staticcheck = true,
-          gofumpt = true,
-        },
-      },
-    }))
-
-    nvim_lsp.rust_analyzer.setup(vim.tbl_deep_extend("force", default_config, {
-      settings = {
-        ["rust-analyzer"] = {
-          diagnostics = { enable = false },
-          cargo = { allFeatures = true },
-          checkOnSave = { command = "clippy" },
-        },
-      },
-    }))
-
-    nvim_lsp.bashls.setup(default_config)
-    nvim_lsp.jsonls.setup(default_config)
-    nvim_lsp.marksman.setup(vim.tbl_deep_extend("force", default_config, {
-      root_dir = util.root_pattern(".git", ".marksman.toml"),
-    }))
   end,
 }
